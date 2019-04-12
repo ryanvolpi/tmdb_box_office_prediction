3347068caabd6b1089eec5dbc5b199395345a44d
ryanvolpi <ryanvolpi@gmail.com>
2019-04-12 13:54:58 -0400

    uplodaing initial file structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project to predict box office revenues using data from TMDB for the Kaggle Competition',
+    author='Ryan Volpi',
+    license='MIT',
+)
